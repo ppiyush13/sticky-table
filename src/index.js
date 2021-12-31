079e8e53056e48221ce83e17e0a4d991f4a35e30
Piyush Lodaya <ppiyush13@gmail.com>
2021-12-31 18:05:53 +0530

    remove dead code

--- src/index.js
+++ src/index.js
@@ -1,8 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { App } from './App';
-//import VirtualGrid from './virtual-grid';
-//import Grid from './tablize';
 
 ReactDOM.render(
   <React.StrictMode>
